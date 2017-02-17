bec373c5830a312bfa49da536cb65f58401bf55a
Alex Rook <rook.alex@gmail.com>
2017-02-17 14:23:27 +0200

    start work with parwise compare

---
+++ math/src/main/java/proeqfa/math/estimation/PairwiseCompare.java
@@ -0,0 +1,16 @@
+package proeqfa.math.estimation;
+
+import org.apache.commons.math3.linear.RealMatrix;
+
+
+/**
+ *
+ * @author moroz
+ */
+public class PairwiseCompare {
+    
+    
+   public RealMatrix getExpectationMatrix(RealMatrix[] marix,OrderNum orderNum){
+       return null;
+   }
+}
