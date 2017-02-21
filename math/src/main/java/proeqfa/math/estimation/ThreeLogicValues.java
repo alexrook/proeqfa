148bad9dd23480341c57039bf739c9b2d5fe373e
Alex Rook <rook.alex@gmail.com>
2017-02-21 12:25:21 +0200

    add ThreeLogicValues
    Документация
        "Методика определения критичности оборудования..."
        Приложение А
        Методология экспертной оценки.
        Вычисление математического ожидания дискретной случайной величины.
        Формула вычисления мат.ожидания в общем виде предполагает n-значений
        для случайной величины,для метода парных сравнений используется только три (больше,равно,меньше)
    
    Добавлен класс расширяющий упорядоченный набор значений и содержащий три значения.

--- math/src/main/java/proeqfa/math/estimation/ThreeLogicValues.java
+++ math/src/main/java/proeqfa/math/estimation/ThreeLogicValues.java
@@ -1,60 +1,61 @@
 package proeqfa.math.estimation;
 
+import java.util.TreeSet;
+
 /**
  * @author moroz
  */
-public class OrderNum {
+public class ThreeLogicValues extends TreeSet<Float> {
 
-    private float more, less, same;
+    private static ThreeLogicValues view1, view2, view3;
 
-    private static OrderNum view1, view2, view3;
-
-    public OrderNum(float more, float less, float same) {
-        this.more = more;
-        this.less = less;
-        this.less = same;
+    public ThreeLogicValues(float more, float less, float same) {
+        add(less);
+        add(more);
+        add(same);
+        //TODO check order of given values and throw error if need ?
     }
 
-    public static OrderNum getView1() {
+    public static ThreeLogicValues getView1() {
         if (view1 != null) {
             return view1;
         } else {
-            view1 = new OrderNum(2, 0, 1);
+            view1 = new ThreeLogicValues(2, 0, 1);
             return view1;
         }
 
     }
 
-    public static OrderNum getView2() {
+    public static ThreeLogicValues getView2() {
         if (view2 != null) {
             return view2;
         } else {
-            view2 = new OrderNum(1, -1, 0);
+            view2 = new ThreeLogicValues(1, -1, 0);
             return view2;
         }
 
     }
 
-    public static OrderNum getView3() {
+    public static ThreeLogicValues getView3() {
         if (view3 != null) {
             return view2;
         } else {
-            view3 = new OrderNum(1, 0, 0.5f);
+            view3 = new ThreeLogicValues(1, 0, 0.5f);
             return view3;
         }
 
     }
 
     public float getMore() {
-        return more;
+        return last();
     }
 
     public float getLess() {
-        return less;
+        return first();
     }
 
     public float getSame() {
-        return same;
+        return lower(getMore());
     }
 
 }
---
+++ math/src/test/java/proeqfa/math/estimation/ThreeLogicValuesTest.java
@@ -0,0 +1,95 @@
+package proeqfa.math.estimation;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author moroz
+ */
+public class ThreeLogicValuesTest {
+
+    float[] VIEW1 = {2, 1, 0},
+            VIEW2 = {1, 0, -1},
+            VIEW3 = {1, 0.5f, 0};
+
+    public ThreeLogicValuesTest() {
+    }
+
+    /**
+     * Test of getView1 method, of class ThreeLogicValues.
+     */
+    @Test
+    public void testGetView1() {
+        System.out.println("getView1");
+        ThreeLogicValues result = ThreeLogicValues.getView1();
+        assertNotNull(result);
+        assertArrayEquals(new float[]{result.getMore(), result.getSame(), result.getLess()},
+                VIEW1, 0);
+
+    }
+
+    /**
+     * Test of getView2 method, of class ThreeLogicValues.
+     */
+    @Test
+    public void testGetView2() {
+        System.out.println("getView2");
+        ThreeLogicValues result = ThreeLogicValues.getView2();
+        assertNotNull(result);
+        assertArrayEquals(new float[]{result.getMore(), result.getSame(), result.getLess()},
+                VIEW2, 0);
+
+    }
+
+    /**
+     * Test of getView3 method, of class ThreeLogicValues.
+     */
+    @Test
+    public void testGetView3() {
+        System.out.println("getView3");
+        ThreeLogicValues result = ThreeLogicValues.getView3();
+        assertNotNull(result);
+        assertArrayEquals(new float[]{result.getMore(), result.getSame(), result.getLess()},
+                VIEW3, 0);
+
+    }
+
+    /**
+     * Test of getMore method, of class ThreeLogicValues.
+     */
+    @Test
+    public void testGetMore() {
+        System.out.println("getMore");
+        float expResult = 5.0F;
+        ThreeLogicValues instance = new ThreeLogicValues(expResult, 2, 1);
+        float result = instance.getMore();
+        assertEquals(expResult, result, 0.0);
+
+    }
+
+    /**
+     * Test of getLess method, of class ThreeLogicValues.
+     */
+    @Test
+    public void testGetLess() {
+        System.out.println("getLess");
+        float expResult = 1.0F;
+        ThreeLogicValues instance = new ThreeLogicValues(3, 2, expResult);
+        float result = instance.getLess();
+        assertEquals(expResult, result, 0.0);
+    }
+
+    /**
+     * Test of getSame method, of class ThreeLogicValues.
+     */
+    @Test
+    public void testGetSame() {
+        System.out.println("getSame");
+        float expResult = 2.0F;
+        ThreeLogicValues instance = new ThreeLogicValues(3, expResult, 1);
+        float result = instance.getSame();
+        assertEquals(expResult, result, 0.0);
+    }
+
+}
