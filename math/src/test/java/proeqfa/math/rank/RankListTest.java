57c20de5d85bff8fdde10bc3e9e7ac9b4bf71890
Alex Rook <rook.alex@gmail.com>
2017-03-10 10:15:47 +0200

    add test data from docs

--- math/src/test/java/proeqfa/math/rank/RankListTest.java
+++ math/src/test/java/proeqfa/math/rank/RankListTest.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.*;
 import static org.junit.Assert.*;
+import static java.lang.System.out;
+import java.util.Arrays;
 
 /**
  * @author moroz
@@ -27,45 +29,89 @@ public class RankListTest {
             @Override
             public List<RankList.RankedObject[]> getOrderedRankedObjectsArrayList() {
                 List<RankList.RankedObject[]> ret = new ArrayList<>(3);
-                RankList.RankedObject[] expert1 = new RankList.RankedObject[3];
-                //expert 1 said O1>O3>O2 --> rank=natural order
-                expert1[0] = new RankList.RankedObject(1);
-                expert1[0].setRank(1);
-                expert1[1] = new RankList.RankedObject(3);
-                expert1[1].setRank(2);
-                expert1[0].setLink(RankList.RankedObjectsLink.MORE);
-                expert1[0].setNextRankedObject(expert1[1]);
-                expert1[2] = new RankList.RankedObject(2);
-                expert1[2].setRank(3);
-                expert1[1].setLink(RankList.RankedObjectsLink.MORE);
-                expert1[1].setNextRankedObject(expert1[2]);
-                ret.add(expert1);
-                
+                //expert A said O1>O3>O2 --> rank=natural order
+                RankList.RankedObject[] expertA = new RankList.RankedObject[3];
+                expertA[0] = new RankList.RankedObject(1);
+                expertA[0].setRank(1);
+                expertA[1] = new RankList.RankedObject(3);
+                expertA[1].setRank(2);
+                expertA[0].setLink(RankList.RankedObjectsLink.MORE);
+                expertA[0].setNextRankedObject(expertA[1]);
+                expertA[2] = new RankList.RankedObject(2);
+                expertA[2].setRank(3);
+                expertA[1].setLink(RankList.RankedObjectsLink.MORE);
+                expertA[1].setNextRankedObject(expertA[2]);
+                ret.add(expertA);
+                //expert B said O3>O2>O1 --> rank=natural order
+                RankList.RankedObject[] expertB = new RankList.RankedObject[3];
+                expertB[0] = new RankList.RankedObject(3);//O3
+                expertB[0].setRank(1);
+                expertB[1] = new RankList.RankedObject(2);//O2
+                expertB[1].setRank(2);
+                expertB[0].setLink(RankList.RankedObjectsLink.MORE);
+                expertB[0].setNextRankedObject(expertB[1]);
+                expertB[2] = new RankList.RankedObject(1);//O1
+                expertB[2].setRank(3);
+                expertB[1].setLink(RankList.RankedObjectsLink.MORE);
+                expertB[1].setNextRankedObject(expertB[2]);
+                ret.add(expertB);
+                //expert C said O1>O2>O3 --> rank=natural order
+                RankList.RankedObject[] expertC = new RankList.RankedObject[3];
+                expertC[0] = new RankList.RankedObject(1);//O1
+                expertC[0].setRank(1);
+                expertC[1] = new RankList.RankedObject(2);//O2
+                expertC[1].setRank(2);
+                expertC[0].setNextRankedObject(expertC[1],
+                        RankList.RankedObjectsLink.MORE);
+                expertC[2] = new RankList.RankedObject(3);//O3
+                expertC[2].setRank(3);
+                expertC[1].setNextRankedObject(expertC[2],
+                        RankList.RankedObjectsLink.MORE);
+                ret.add(expertC);
                 return ret;
             }
+
         };
     }
 
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of fromArray method, of class RankList.
-     */
     @Test
-    public void testFromArray() {
+    public void testFromArray_toRankVector_01() {
         System.out.println("fromArray");
-        fail("The test case is a prototype.");
+        RankList.RankedObject[] rankedObjects
+                = docData.getOrderedRankedObjectsArrayList().get(0);
+        RankList instance = RankList.fromArray(rankedObjects);
+        //  out.println(instance);
+        assertEquals(rankedObjects.length, instance.size());
+        double[] expectedVector = getRankVector(rankedObjects);
+        out.println(Arrays.toString(expectedVector));
+        assertArrayEquals(expectedVector, instance.toRankedObjectVector(), 0);
+
+        rankedObjects
+                = docData.getOrderedRankedObjectsArrayList().get(1);
+        instance = RankList.fromArray(rankedObjects);
+        //  out.println(instance);
+        assertEquals(rankedObjects.length, instance.size());
+        expectedVector = getRankVector(rankedObjects);
+        out.println(Arrays.toString(expectedVector));
+        assertArrayEquals(expectedVector, instance.toRankedObjectVector(), 0);
+
+        rankedObjects
+                = docData.getOrderedRankedObjectsArrayList().get(2);
+        instance = RankList.fromArray(rankedObjects);
+        //  out.println(instance);
+        assertEquals(rankedObjects.length, instance.size());
+        expectedVector = getRankVector(rankedObjects);
+        out.println(Arrays.toString(expectedVector));
+        assertArrayEquals(expectedVector, instance.toRankedObjectVector(), 0);
+
     }
 
-    /**
-     * Test of toRankedObjectVector method, of class RankList.
-     */
-    @Test
-    public void testToRankedObjectVector() {
-        System.out.println("toRankedObjectVector");
-        fail("The test case is a prototype.");
+    public static double[] getRankVector(RankList.RankedObject[] rankedObjects) {
+        double[] ret = new double[rankedObjects.length];
+        for (int i = 0; i < rankedObjects.length; i++) {
+            ret[rankedObjects[i].getRankedObjectId() - 1] = rankedObjects[i].getRank();
+        }
+        return ret;
     }
 
 }
