5181baac8527b55af5f310d3a3d44637a941b349
Alex Rook <rook.alex@gmail.com>
2017-03-16 13:46:11 +0200

    add RankMatrix & test

--- math/src/main/java/proeqfa/math/rank/RankMatrix.java
+++ math/src/main/java/proeqfa/math/rank/RankMatrix.java
@@ -1,7 +1,86 @@
 package proeqfa.math.rank;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by moroz on 16.03.17.
  */
 public class RankMatrix {
+
+    private List<RankChain> expertChains = new ArrayList<>(5);
+
+    private int rankedObjectsCount;
+
+    public RankMatrix(int rankedObjectsCount) {
+        this.rankedObjectsCount = rankedObjectsCount;
+    }
+
+    /**
+     * @param rankChain
+     */
+    public void addRankChain(RankChain rankChain) {
+        if (rankChain.size() == this.rankedObjectsCount) {
+            expertChains.add(rankChain);
+        } else {
+            throw new IllegalArgumentException("The rank chain has a different number of objects");
+        }
+    }
+
+    /**
+     * @return
+     */
+    public double[] getRankedObjectsSums() {
+        double[] ret = new double[rankedObjectsCount];
+        for (RankChain rc : expertChains) {
+            double[] vec = rc.toRankedObjectVector();
+            assert vec.length == ret.length;
+            //objects in vector sorted by their id
+            for (int i = 0; i < vec.length; i++) {
+                ret[i] += vec[i] * rc.getExpertCompetenceFactor();
+            }
+
+        }
+        return ret;
+    }
+
+    /**
+     * @return
+     */
+    public RankChain.RankedObject getMaxRankObject() {
+        double[] sums = getRankedObjectsSums();
+        int id = -1;
+        double rank = Double.MIN_VALUE;
+        for (int i = 0; i < sums.length; i++) {
+            if (rank < sums[i]) {
+                rank = sums[i];
+                id = i + 1;
+            }
+        }
+        RankChain.RankedObject ret = new RankChain.RankedObject(id);
+        ret.setRank(rank);
+        return ret;
+    }
+
+    /**
+     * @return
+     */
+    public RankChain.RankedObject getMinRankObject() {
+        double[] sums = getRankedObjectsSums();
+        int id = -1;
+        double rank = Double.MAX_VALUE;
+        for (int i = 0; i < sums.length; i++) {
+            if (rank > sums[i]) {
+                rank = sums[i];
+                id = i + 1;
+            }
+        }
+        RankChain.RankedObject ret = new RankChain.RankedObject(id);
+        ret.setRank(rank);
+        return ret;
+    }
+
+    public int getRankedObjectsCount() {
+        return rankedObjectsCount;
+    }
 }
--- math/src/test/java/proeqfa/math/rank/RankMatrixTest.java
+++ math/src/test/java/proeqfa/math/rank/RankMatrixTest.java
@@ -1,7 +1,96 @@
 package proeqfa.math.rank;
 
+import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.*;
+import static java.lang.System.out;
+
 /**
  * Created by moroz on 16.03.17.
  */
 public class RankMatrixTest {
+
+    private List<RankChain> docData;
+
+    @Before
+    public void setUp() {
+        //set rank here for testing purposes
+        docData = new ArrayList<>(3);
+        //expert A said O1>O3>O2 --> rank=natural order
+        RankChain.RankedObject[] expertA = new RankChain.RankedObject[3];
+        expertA[0] = new RankChain.RankedObject(1);
+        expertA[0].setRank(1);
+        expertA[1] = new RankChain.RankedObject(3);
+        expertA[1].setRank(2);
+        expertA[1].setPreviousObjectRel(RankChain.RankedObjectsLink.MORE);
+
+        expertA[2] = new RankChain.RankedObject(2);
+        expertA[2].setRank(3);
+        expertA[2].setPreviousObjectRel(RankChain.RankedObjectsLink.MORE);
+
+        RankChain rankChainA = RankChain.fromArray(expertA, new NaturalOrderPosition2Rank());
+        rankChainA.setExpertCompetenceFactor(0.5);
+        docData.add(rankChainA);
+
+        //expert B said O3>O2>O1 --> rank=natural order
+        RankChain.RankedObject[] expertB = new RankChain.RankedObject[3];
+        expertB[0] = new RankChain.RankedObject(3);//O3
+        expertB[0].setRank(1);
+
+        expertB[1] = new RankChain.RankedObject(2);//O2
+        expertB[1].setRank(2);
+        expertB[1].setPreviousObjectRel(RankChain.RankedObjectsLink.MORE);
+
+        expertB[2] = new RankChain.RankedObject(1);//O1
+        expertB[2].setRank(3);
+        expertB[2].setPreviousObjectRel(RankChain.RankedObjectsLink.MORE);
+
+        RankChain rankChainB = RankChain.fromArray(expertB, new NaturalOrderPosition2Rank());
+        rankChainB.setExpertCompetenceFactor(0.01);
+        docData.add(rankChainB);
+
+        //expert C said O1>O2>O3 --> rank=natural order
+        RankChain.RankedObject[] expertC = new RankChain.RankedObject[3];
+        expertC[0] = new RankChain.RankedObject(1);//O1
+        expertC[0].setRank(1);
+        expertC[1] = new RankChain.RankedObject(2);//O2
+        expertC[1].setRank(2);
+        expertC[1].setPreviousObjectRel(RankChain.RankedObjectsLink.MORE);
+
+        expertC[2] = new RankChain.RankedObject(3);//O3
+        expertC[2].setRank(3);
+        expertC[2].setPreviousObjectRel(RankChain.RankedObjectsLink.MORE);
+
+        RankChain rankChainC = RankChain.fromArray(expertC, new NaturalOrderPosition2Rank());
+        rankChainC.setExpertCompetenceFactor(0.49);
+        docData.add(rankChainC);
+
+    }
+
+    @Test
+    public void testGetRankedObjectsSums01() {
+        out.println("testGetRankedObjectsSums");
+        RankMatrix instance = new RankMatrix(3);
+        for (RankChain rankChain : docData) {
+            assertEquals(rankChain.size(), instance.getRankedObjectsCount());
+            instance.addRankChain(rankChain);
+        }
+
+        double[] expectedRanks = new double[]{1.02, 2.5, 2.48};//see project docs
+        double[] actualRanks = instance.getRankedObjectsSums();
+        out.println("actual ranks: "+Arrays.toString(actualRanks));
+        assertArrayEquals(expectedRanks, actualRanks, 0);
+        double expMaxRank=expectedRanks[1];
+        assertEquals(2,instance.getMaxRankObject().getRankedObjectId());
+        assertEquals(expMaxRank,instance.getMaxRankObject().getRank(),0);
+
+        double expMinRank=expectedRanks[0];
+        assertEquals(1,instance.getMinRankObject().getRankedObjectId());
+        assertEquals(expMinRank,instance.getMinRankObject().getRank(),0);
+
+    }
 }
