b4a19785027429342fdc253175173f4a0cac46e5
Alex Rook <rook.alex@gmail.com>
2017-03-20 12:19:14 +0200

    fix toPairwiseCompareMatrix  alg. need more tests

--- math/src/main/java/proeqfa/math/rankestjoin/RankEstimation.java
+++ math/src/main/java/proeqfa/math/rankestjoin/RankEstimation.java
@@ -5,68 +5,73 @@ import proeqfa.math.estimation.PairwiseCompareMatrix;
 import proeqfa.math.estimation.ThreeLogicValues;
 import proeqfa.math.rank.RankChain;
 
-import static java.lang.System.out;
+import java.util.List;
+
+
 
 /**
  * Created by moroz on 17.03.17.
  */
 public class RankEstimation {
 
-    private int rankedObjectsCount;
-    private ThreeLogicValues logicValues;
-    EstimationMatrix estimationMatrix;
+    private EstimationMatrix estimationMatrix;
 
 
     public RankEstimation(int rankedObjectsCount, ThreeLogicValues logicValues) {
-        this.rankedObjectsCount = rankedObjectsCount;
         estimationMatrix =
                 new EstimationMatrix(rankedObjectsCount, logicValues);
     }
 
     public void add(RankChain rankChain) {
-        if (rankChain.size() != rankedObjectsCount) {
+        if (rankChain.size() != estimationMatrix.getObjectsCount()) {
             throw new IllegalArgumentException("ranked chain has different object size");
         }
 
-        estimationMatrix.addPairwiseCompare(toPairwiseCompareMatrix(rankChain));
+        estimationMatrix.addPairwiseCompare(
+                toPairwiseCompareMatrix(rankChain,
+                        estimationMatrix.getObjectsCount(),
+                        estimationMatrix.getLogicValues()));
 
     }
 
-    /*TODO: protected only for tests*/
-    protected PairwiseCompareMatrix toPairwiseCompareMatrix(RankChain rankChain) {
+
+    public static PairwiseCompareMatrix toPairwiseCompareMatrix(RankChain rankChain,
+                                                            int objectCount,
+                                                            ThreeLogicValues logicValues) {
         PairwiseCompareMatrix ret = new PairwiseCompareMatrix(
-                estimationMatrix.getObjectsCount(),
-                estimationMatrix.getLogicValues());
-        int i, j;
+                objectCount,
+                logicValues);
+        int i, j, k = 0;
 
         for (RankChain.RankedObject roi : rankChain) {
 
-            i = roi.getId() - 1;
+            k++;
 
+            i = roi.getId() - 1;
             boolean alreadyMore = false;
 
-            for (RankChain.RankedObject roj : rankChain) {
-                j = roj.getId() - 1;
-                if (i != j) {
-                    switch (roj.getPreviousObjectRel()) {
-                        case MORE: {
-                            ret.setMore(i, j);
-                            alreadyMore = true;
-                            break;
-                        }
-                        case SAME: {
-                            if (alreadyMore) {
-                                ret.setMore(i, j);
-                            } else {
-                                ret.setSame(i, j);
-                            }
-                        }
-                        default:
-                            ret.setSame(i, j);
-                    }
-                }
-            }
+            List<RankChain.RankedObject> head = rankChain.getHead(k);
 
+            for (RankChain.RankedObject roj : head) {
+                j = roj.getId() - 1;
+                switch (roj.getPreviousObjectRel()) {
+                    case MORE: {
+                        ret.setMore(i, j);
+                        alreadyMore = true;
+                        break;
+                    }
+                    case SAME: {
+                        if (alreadyMore) {
+                            ret.setMore(i, j);
+                        } else {
+                            ret.setSame(i, j);
+                        }
+                    }
+//                        default:
+//                            ret.setSame(i, j);
+                }
+
+            }
         }
 
         return ret;
--- math/src/test/java/proeqfa/math/rankestjoin/RankEstimationTest.java
+++ math/src/test/java/proeqfa/math/rankestjoin/RankEstimationTest.java
@@ -1,10 +1,11 @@
 package proeqfa.math.rankestjoin;
 
+import org.apache.commons.math3.linear.MatrixUtils;
+import org.apache.commons.math3.linear.RealMatrix;
+import org.junit.Before;
 import org.junit.Test;
 import proeqfa.math.commons.Array2DUtils;
-import proeqfa.math.estimation.PairwiseCompareMatrix;
-import proeqfa.math.estimation.TestUtils;
-import proeqfa.math.estimation.ThreeLogicValues;
+import proeqfa.math.estimation.*;
 import proeqfa.math.rank.NaturalOrderPosition2Rank;
 import proeqfa.math.rank.RankChain;
 import proeqfa.math.rank.RankChainTest;
@@ -17,47 +18,98 @@ import static org.junit.Assert.*;
  */
 public class RankEstimationTest {
 
-    @Test
-    public void add() throws Exception {
-        //todo
-    }
+    //-- doc рис. А.6
+    final int DD_OBJ_COUNT = 3; //dd -> doc data
+    final ThreeLogicValues DD_LOGIC_VALUES = ThreeLogicValues.getView3();
+    private double DD_EVALUATION_RATE = 0.001;
+    RankChain dd_expert1, dd_expert2, dd_expert3;
 
-    @Test
-    public void toPairwiseCompareMatrix_01() {
+    RealMatrix dd_ri_vector =
+            MatrixUtils.createColumnRealMatrix(new double[]{0.5, 0.349, 0.151}); // relative importance vector
+
+    double[][] dd_pw_expected1 = { //pw -> expected pairwise compare matrix
+            {0.5, 1, 1},
+            {0, 0.5, 1},
+            {0, 0, 0.5}
+    };
+    double[][] dd_pw_expected2 = {
+            {0.5, 1, 1},
+            {0, 0.5, 0},
+            {0, 1, 0.5}
+    };
+    double[][] dd_pw_expected3 = {
+            {0.5, 0, 1},
+            {1, 0.5, 1},
+            {0, 0, 0.5}
+    };
+    //---
+
+    @Before
+    public void setUp() {
         //doc рис. А.6
-        RankChain expert1 = RankChain.fromArray(
+        dd_expert1 = RankChain.fromArray(
                 RankChainTest.getRankChain("O1(1) >O2(2) >O3(3)")
                 , new NaturalOrderPosition2Rank());
 
-        RankChain expert2 = RankChain.fromArray(
+        dd_expert2 = RankChain.fromArray(
                 RankChainTest.getRankChain("O1(1) >O3(2) >O2(3)")
                 , new NaturalOrderPosition2Rank());
 
-        RankChain expert3 = RankChain.fromArray(
+        dd_expert3 = RankChain.fromArray(
                 RankChainTest.getRankChain("O2(1) >O1(2) >O3(3)")
                 , new NaturalOrderPosition2Rank());
+    }
 
-        RankEstimation instance = new RankEstimation(3,
-                ThreeLogicValues.getView3());
 
-        PairwiseCompareMatrix actualMatrix1 = instance.toPairwiseCompareMatrix(expert1);
-        TestUtils.printPairwiseMatrix(actualMatrix1, out);
-//        Double[][] actual1 = actualMatrix1.getMatrix();
-//        double[][] expected1 = {
-//                {0.5, 1, 1},
-//                {0, 0.5, 1},
-//                {0, 0, 0.5}
-//        };
-//        TestUtils.printMatrix(actual1, out);
-//        TestUtils.printMatrix(expected1, out);
-//
-//        TestUtils.assert2DArrayEquals(expected1, Array2DUtils.toPrimitive(actual1), 0d);
+    @Test
+    public void test_dd_to_RelativeImportanceVector() throws Exception {
 
+        RankEstimation instance = new RankEstimation(DD_OBJ_COUNT, DD_LOGIC_VALUES);
+        instance.add(dd_expert1);
+        instance.add(dd_expert2);
+        instance.add(dd_expert3);
+
+        EstimationMatrix estM = instance.get();
+        estM.calculate();
+//        out.println("---");
+//        TestUtils.printMatrix(estM.getResultMatrix(), out);
+
+        RelativeImportanceVector vector = new RelativeImportanceVector(DD_OBJ_COUNT, DD_EVALUATION_RATE);
+        vector.calculate(estM.getResultMatrix());
+        RealMatrix actualVector = vector.getRelativeImportanceVector();
+        TestUtils.printMatrix(actualVector.getData(), out, 3, 3);
+        TestUtils.assert2DArrayEquals(dd_ri_vector.getData(),
+                actualVector.getData(), DD_EVALUATION_RATE);
     }
 
     @Test
-    public void get() throws Exception {
-        //todo
+    public void test_dd_toPairwiseCompareMatrix01() {
+
+        PairwiseCompareMatrix actPaMatrix1 =
+                RankEstimation.toPairwiseCompareMatrix(dd_expert1, DD_OBJ_COUNT, DD_LOGIC_VALUES);
+        PairwiseCompareMatrix actPaMatrix2 =
+                RankEstimation.toPairwiseCompareMatrix(dd_expert2, DD_OBJ_COUNT, DD_LOGIC_VALUES);
+        PairwiseCompareMatrix actPaMatrix3 =
+                RankEstimation.toPairwiseCompareMatrix(dd_expert3, DD_OBJ_COUNT, DD_LOGIC_VALUES);
+
+        out.println("pairwise matrix from expert1:");
+        TestUtils.printPairwiseMatrix(actPaMatrix1, out);
+        out.println("pairwise matrix from expert2:");
+        TestUtils.printPairwiseMatrix(actPaMatrix2, out);
+        out.println("pairwise matrix from expert3:");
+        TestUtils.printPairwiseMatrix(actPaMatrix3, out);
+
+
+        TestUtils.assert2DArrayEquals(
+                dd_pw_expected1,
+                Array2DUtils.toPrimitive(actPaMatrix1.getMatrix()), 0d);
+        TestUtils.assert2DArrayEquals(
+                dd_pw_expected2,
+                Array2DUtils.toPrimitive(actPaMatrix2.getMatrix()), 0d);
+        TestUtils.assert2DArrayEquals(
+                dd_pw_expected3,
+                Array2DUtils.toPrimitive(actPaMatrix3.getMatrix()), 0d);
+
     }
 
 }
