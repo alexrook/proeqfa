bab0507c333403c1f6cfba935cea686519d1eb3e
Alex Rook <rook.alex@gmail.com>
2017-03-21 12:58:25 +0200

    add some userdata tests

--- math/src/test/java/proeqfa/math/TestBase.java
+++ math/src/test/java/proeqfa/math/TestBase.java
@@ -9,25 +9,23 @@ import java.io.PrintStream;
 /**
  * Created by moroz on 20.03.17.
  */
-public  class TestBase {
+public class TestBase {
 
     protected Logger log;
     protected ByteArrayOutputStream baos;
     protected PrintStream out;
 
 
-    public TestBase(){
-        log= LogManager.getLogger(this.getClass().getName());
+    public TestBase() {
+        log = LogManager.getLogger(this.getClass().getName());
     }
 
-    public  void setUp(){
+    public void setUp() {
         baos = new ByteArrayOutputStream();
         out = new PrintStream(baos);
-        out.println("test base");
-
     }
 
-    public void tearDown(){
+    public void tearDown() {
         log.debug(new String(baos.toByteArray(), java.nio.charset.StandardCharsets.UTF_8));
     }
 }
--- math/src/test/java/proeqfa/math/rankestjoin/RankEstimationTest.java
+++ math/src/test/java/proeqfa/math/rankestjoin/RankEstimationTest.java
@@ -2,19 +2,15 @@ package proeqfa.math.rankestjoin;
 
 import org.apache.commons.math3.linear.MatrixUtils;
 import org.apache.commons.math3.linear.RealMatrix;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import proeqfa.math.TestBase;
 import proeqfa.math.commons.Array2DUtils;
 import proeqfa.math.estimation.*;
 import proeqfa.math.rank.NaturalOrderPosition2Rank;
 import proeqfa.math.rank.RankChain;
 import proeqfa.math.rank.RankChainTest;
-
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 
 /**
@@ -46,6 +42,36 @@ public class RankEstimationTest extends TestBase {
             {1, 0.5, 1},
             {0, 0, 0.5}
     };
+    //users data
+    final int PD_OBJ_COUNT = 3;
+    final ThreeLogicValues PD_LOGIC_VALUES = ThreeLogicValues.getView3();
+    RankChain pd_expert1, pd_expert2, pd_expert3, pd_expert4;
+    double[][] pd_pw_expected1 = { //pw -> expected pairwise compare matrix
+            {0.5, 1, 1},
+            {0, 0.5, 1},
+            {0, 0, 0.5}
+    };
+    double[][] pd_pw_expected2 = {
+            {0.5, 0, 1},
+            {1, 0.5, 1},
+            {0, 0, 0.5}
+    };
+    double[][] pd_pw_expected3 = {
+            {0.5, 1, 1},
+            {0, 0.5, 0},
+            {0, 1, 0.5}
+    };
+    double[][] pd_pw_expected4 = {
+            {0.5, 1, 1},
+            {0, 0.5, 1},
+            {0, 0, 0.5}
+    };
+    //
+    double[][] pd_estimationmatrix_expected = {
+            {0.5, (3 / 4d), 1d},
+            {(1 / 4d), 0.5, (3 / 4d)},
+            {0d, (1 / 4d), 0.5}
+    };
     //---
 
     @Before
@@ -63,6 +89,23 @@ public class RankEstimationTest extends TestBase {
 
         dd_expert3 = RankChain.fromArray(
                 RankChainTest.getRankChain("O2(1) >O1(2) >O3(3)")
+                , new NaturalOrderPosition2Rank());
+
+        /*users test data*/
+        pd_expert1 = RankChain.fromArray(
+                RankChainTest.getRankChain("O1(1) >O2(2) >O3(3)")
+                , new NaturalOrderPosition2Rank());
+
+        pd_expert2 = RankChain.fromArray(
+                RankChainTest.getRankChain("O2(1) >O1(2) >O3(3)")
+                , new NaturalOrderPosition2Rank());
+
+        pd_expert3 = RankChain.fromArray(
+                RankChainTest.getRankChain("O1(1) >O3(2) >O2(3)")
+                , new NaturalOrderPosition2Rank());
+
+        pd_expert4 = RankChain.fromArray(
+                RankChainTest.getRankChain("O1(1) >O2(2) >O3(3)")
                 , new NaturalOrderPosition2Rank());
 
     }
@@ -133,4 +176,51 @@ public class RankEstimationTest extends TestBase {
                 Array2DUtils.toPrimitive(actPaMatrix3.getMatrix()), 0d);
     }
 
+    @Test
+    public void test_userdata_toPairwiseCompareMatrix01() {
+
+        PairwiseCompareMatrix actPaMatrix1 =
+                RankEstimation.toPairwiseCompareMatrix(pd_expert1, PD_OBJ_COUNT, PD_LOGIC_VALUES);
+        PairwiseCompareMatrix actPaMatrix2 =
+                RankEstimation.toPairwiseCompareMatrix(pd_expert2, PD_OBJ_COUNT, PD_LOGIC_VALUES);
+        PairwiseCompareMatrix actPaMatrix3 =
+                RankEstimation.toPairwiseCompareMatrix(pd_expert3, PD_OBJ_COUNT, PD_LOGIC_VALUES);
+        PairwiseCompareMatrix actPaMatrix4 =
+                RankEstimation.toPairwiseCompareMatrix(pd_expert4, PD_OBJ_COUNT, PD_LOGIC_VALUES);
+
+        out.println("pairwise matrix from expert1:");
+        TestUtils.printPairwiseMatrix(actPaMatrix1, out);
+        out.println("pairwise matrix from expert2:");
+        TestUtils.printPairwiseMatrix(actPaMatrix2, out);
+        out.println("pairwise matrix from expert3:");
+        TestUtils.printPairwiseMatrix(actPaMatrix3, out);
+        out.println("pairwise matrix from expert4:");
+        TestUtils.printPairwiseMatrix(actPaMatrix4, out);
+
+
+        TestUtils.assert2DArrayEquals(
+                pd_pw_expected1,
+                Array2DUtils.toPrimitive(actPaMatrix1.getMatrix()), 0d);
+        TestUtils.assert2DArrayEquals(
+                pd_pw_expected2,
+                Array2DUtils.toPrimitive(actPaMatrix2.getMatrix()), 0d);
+        TestUtils.assert2DArrayEquals(
+                pd_pw_expected3,
+                Array2DUtils.toPrimitive(actPaMatrix3.getMatrix()), 0d);
+        TestUtils.assert2DArrayEquals(
+                pd_pw_expected4,
+                Array2DUtils.toPrimitive(actPaMatrix4.getMatrix()), 0d);
+
+        EstimationMatrix em = new EstimationMatrix(PD_OBJ_COUNT, PD_LOGIC_VALUES);
+
+        em.addPairwiseCompare(actPaMatrix1);
+        em.addPairwiseCompare(actPaMatrix2);
+        em.addPairwiseCompare(actPaMatrix3);
+        em.addPairwiseCompare(actPaMatrix4);
+        em.calculate();
+        // TestUtils.printMatrix(em.getResultMatrix(), out);
+        TestUtils.assert2DArrayEquals(
+                pd_estimationmatrix_expected,
+                em.getResultMatrix(), 0d);
+    }
 }
